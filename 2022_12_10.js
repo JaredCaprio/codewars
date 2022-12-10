2e16c2f231bf141d0c28ad19bb9ab16fbe3b6518
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-12-10 12:03:20 -0500

    Update 2022_12_10.js

--- 2022_12_10.js
+++ 2022_12_10.js
@@ -7,7 +7,6 @@ Only lower case letters will be used (a-z). No punctuation or digits will be inc
 Performance needs to be considered.
 
 Examples
-
 scramble('rkqodlw', 'world') ==> True
 scramble('cedewaraaossoqqyt', 'codewars') ==> True
 scramble('katas', 'steak') ==> False
