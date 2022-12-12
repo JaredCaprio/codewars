d886d8dd9e512d78f23557cb164dbbb314ced92c
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2022-12-12 12:05:28 -0500

    2022-12-12

--- 2022_12_12.js
+++ 2022_12_12.js
@@ -33,3 +33,26 @@ function sumStr(a, b) {
   return sum.toString();
 }
 //</3>
+
+/*<4> https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+
+When provided with a number between 0-9, return it in words.
+Input :: 1
+Output :: "One".
+*/
+function switchItUp(number) {
+  let lNm = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+  };
+  return lNm[number];
+}
+//</4>
