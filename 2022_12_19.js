4c120965e3129c7c495046f103133d9209e4cfc1
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2022-12-19 11:32:42 -0500

    2022-12-19

---
+++ 2022_12_19.js
@@ -0,0 +1,29 @@
+/*<1> https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
+A function is pure when:
+
+It always return the same value given the same arguments (it doesn't update or 
+depend on out of the scope variables);
+Evaluation of the result does not cause side effect (mutations...) or output
+
+Task
+
+You are given a function that is impure, and your job is to purify it. 
+This function must return a new array where each value is itself plus 2
+ times the "modifier", which is provided as a property of the options object.
+
+Example:
+Array = 1, 2, 3
+Modifier = 5
+
+Should return = 11, 12, 13
+ */
+function solution(arr, options) {
+  let modifier = options.modifier;
+
+  let newArr = arr.map((item) => {
+    return (item += 2 * modifier);
+  });
+
+  return newArr;
+}
+//</1>
