da39d3b09af7d4d3c6534dd885b9b62eccc90aa7
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2022-12-29 10:37:37 -0500

    2022-12-29

---
+++ 2022_12_29.js
@@ -0,0 +1,28 @@
+/*<1>https://www.codewars.com/kata/585a033e3a36cdc50a00011c/train/javascript
+Your task is to return an output string that translates an input string s by
+ replacing each character in s with a number representing the number of times
+  that character occurs in s and separating each number with the sep character(s).
+
+Example (s, sep --> Output)
+
+"hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+"19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+"^^^**$"     , "x" --> "3x3x3x2x2x1"
+*/
+function freqSeq(str, sep) {
+  let charMap = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (!charMap[str[i]]) {
+      charMap[str[i]] = 1;
+    } else {
+      charMap[str[i]] += 1;
+    }
+  }
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    result.push(charMap[str[i]]);
+  }
+  return result.join(sep);
+}
+//</1>
