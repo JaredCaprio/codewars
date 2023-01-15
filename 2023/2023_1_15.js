5da2f427fb9aae20dc8a94f0fdefb67aee0b9700
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-01-15 09:58:35 -0500

    2023-1-15

---
+++ 2023/2023_1_15.js
@@ -0,0 +1,31 @@
+/*<1> https://www.codewars.com/kata/52449b062fb80683ec000024/train/javascript
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+    It must start with a hashtag (#).
+    All words must have their first letter capitalized.
+    If the final result is longer than 140 chars it must return false.
+    If the input or the result is an empty string it must return false.
+
+Examples
+
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+*/
+function generateHashtag(str) {
+  if (str.trim().length < 1) return str.trim();
+  let result = [];
+  let regex = new RegExp("[ ]+");
+  let capSplitJoin = str
+    .trim()
+    .split(regex)
+    .forEach((word) => {
+      result.push(word[0].toUpperCase() + word.slice(1));
+    });
+  let hashTag = "#" + result.join("");
+  return hashTag.length < 141 ? hashTag : "Too long";
+}
+//</1>
