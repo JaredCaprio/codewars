cc66bd9b2d1ebc74698326a31014215b9d4049d6
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-03-10 08:51:43 -0500

    2023_3_10

---
+++ 2023/2023_3_10.js
@@ -0,0 +1,16 @@
+/* <1>https://www.codewars.com/kata/572b6b2772a38bc1e700007a/train/javascript
+You'll be given a string, and have to return the sum of all characters as an int. 
+The function should be able to handle all printable ASCII characters.
+
+Examples:
+uniTotal("a") == 97
+uniTotal("aaa") == 291
+*/
+function uniTotal(string) {
+  let total = 0;
+  for (let i = 0; i < string.length; i++) {
+    total += string.charCodeAt(i);
+  }
+  return total;
+}
+//</1>
