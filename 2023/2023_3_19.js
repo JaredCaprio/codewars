8aefb56e7565abaab025ab26a4036a90146b0377
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-03-19 10:01:57 -0400

    2023-3-19

---
+++ 2023/2023_3_19.js
@@ -0,0 +1,9 @@
+/* <1> https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/javascript
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+*/
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
+//</1>
