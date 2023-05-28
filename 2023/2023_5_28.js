44d0176a0926760eb00b2d3ef3c87f1aacca1d97
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-05-28 10:17:07 -0400

    2023-5-28

---
+++ 2023/2023_5_28.js
@@ -0,0 +1,53 @@
+/* <1> https://leetcode.com/problems/isomorphic-strings/description
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order 
+of characters. No two characters may map to the same character, but a character may map to itself. 
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true 
+
+Constraints:
+
+    1 <= s.length <= 5 * 104
+    t.length == s.length
+    s and t consist of any valid ascii character.
+*/
+
+const isIsomorphic = (s, t) => {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const mapS = {};
+  const mapT = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let sChar = s[i];
+    let tChar = t[i];
+
+    if (mapS[sChar] == undefined) {
+      mapS[sChar] = tChar;
+    }
+    if (mapT[tChar] == undefined) {
+      mapT[tChar] = sChar;
+    }
+    if (mapS[sChar] !== tChar || mapT[tChar] !== sChar) {
+      return false;
+    }
+  }
+  return true;
+};
+//</1>
