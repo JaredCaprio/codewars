83a48a9f3f14dfd2efce77fcb2d065c85bb62f0f
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-06-21 11:51:46 -0400

    2023-6-12

--- 2023/2023_6_21.js
+++ 2023/2023_6_21.js
@@ -1,20 +1,41 @@
-const mergeAlternately = (word1, word2) => {
-  let i = 0;
-  let j = 0;
+/* <1> https://leetcode.com/problems/greatest-common-divisor-of-strings/description
+1071. Greatest Common Divisor of Strings
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
 
-  let res = [];
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2. 
 
-  while (i < word1.length && j < word2.length) {
-    res.push(word1[i]);
-    res.push(word2[j]);
-    i++;
-    j++;
-  }
+Example 1:
 
-  res.push(word1.substring(i));
-  res.push(word2.substring(j));
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
 
-  return res.join("");
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: "" 
+
+Constraints:
+
+    1 <= str1.length, str2.length <= 1000
+    str1 and str2 consist of English uppercase letters.
+*/
+var gcdOfStrings = function (str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+
+  if (str1 + str2 !== str2 + str1) return "";
+
+  let gcd = function (x, y) {
+    if (!y) return x;
+    return gcd(y, x % y);
+  };
+
+  let div = gcd(len1, len2);
+  return str1.slice(0, div);
 };
-
-console.log(mergeAlternately("abc", "xyz"));
+//</1>
