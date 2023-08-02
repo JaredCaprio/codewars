4cee407551fcf1c7f331be8e922f9b07a4ec3eed
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-08-02 08:54:48 -0400

    2023-8-2

--- 2023/2023_8_2.js
+++ 2023/2023_8_2.js
@@ -1,28 +1,36 @@
-/* <1>
+/* <1> https://www.codewars.com/kata/57fcaed83206fb15fd00027a/train/javascript
+Task
 
+Write a method, that replaces every nth char oldValue with char newValue.
+Inputs
+    text: the string to modify
+    n: change the target letter every nth occurrencies
+    old_value (or similar): the targetted character
+    new_value (or similar): the character to use as replacement
+
+Note for untyped languages: all inputs are always valid and of their expected type.
+Rules
+    Your method has to be case sensitive!
+    If n is 0 or negative or if it is larger than the count of the oldValue, return the original text without a change.
+
+Example:
+n:         2
+old_value: 'a'
+new_value: 'o'
+"Vader said: No, I am your father!"
+  1     2          3        4       -> 2nd and 4th occurence are replaced
+"Vader soid: No, I am your fother!"
+
+As you can see in the example: The first changed is the 2nd 'a'. 
+So the start is always at the nth suitable char and not at the first!
 */
 
-//PREP
-
-//P (text: string to mod, n: replace every nth letter with newVal,
-// oldValue: the letter to be replaced, newValue: letter to replace)
-
-//R
-//return modified string
-
-//E
-
-console.log(replaceNth("Vader said: No, I am your father!", 2, "a", "o"));
-
 function replaceNth(text, n, oldValue, newValue) {
-  //handle edge case of 0 or negative input
   if (n <= 0) return text;
 
-  // create couter var to keep track of every occurence of old val
   let counter = 0;
   let newStr = "";
 
-  //loop over string
   for (let i = 0; i < text.length; i++) {
     if (text[i] === oldValue && counter === n - 1) {
       newStr += newValue;
@@ -35,6 +43,5 @@ function replaceNth(text, n, oldValue, newValue) {
     }
   }
   return newStr;
-  //return new string
 }
 //</1>
