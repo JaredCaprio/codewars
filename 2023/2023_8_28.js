44c7d50e3e55258df02d8ad51edd9a3ab3c09112
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-08-28 09:12:00 -0400

    2023-8-28

---
+++ 2023/2023_8_28.js
@@ -0,0 +1,44 @@
+/* <1> https://www.codewars.com/kata/57ab2d6072292dbf7c000039/train/javascript
+There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+Your task is to change the letters with diacritics:
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+
+and print out the string without the use of the Polish letters.
+
+For example:
+"Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+*/
+function correctPolishLetters(string) {
+  const charRep = {
+    ą: "a",
+    ć: "c",
+    ę: "e",
+    ł: "l",
+    ń: "n",
+    ó: "o",
+    ś: "s",
+    ź: "z",
+    ż: "z",
+  };
+
+  let res = "";
+
+  for (let i = 0; i < string.length; i++) {
+    if (charRep[string[i]]) {
+      res += charRep[string[i]];
+    } else {
+      res += string[i];
+    }
+  }
+  return res;
+}
+//</1>
