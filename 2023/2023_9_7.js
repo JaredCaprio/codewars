c99b3381e11904f3d05b47e979edd14bc4b7095f
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-09-09 08:51:02 -0400

    2023-9-9

--- 2023/2023_9_7.js
+++ 2023/2023_9_7.js
@@ -1,5 +1,4 @@
-/* <1>
-
+/* <1> https://www.codewars.com/kata/latest/my-languages?beta=false
 Complete the function which will return the area of a circle with the given radius.
 
 Round the returned number to two decimal places (except for Haskell). If the radius is not positive or not a number, return false.
@@ -12,24 +11,10 @@ circleArea(43.2673);     //returns 5881.25
 circleArea(68);          //returns 14526.72
 circleArea("number");    //returns false
 
-
-
 */
-
-//Parameters: (radius: a positive integer)
-//Return false if radius is less than or equal to zero
-//Examples
-
 var circleArea = function (radius) {
   if (radius <= 0) return false;
   let area = Math.PI * radius ** 2;
   return area.toFixed(2);
 };
-
-console.log(circleArea(43.2673));
-console.log(circleArea(68));
-console.log(circleArea(-1485.86));
-console.log(circleArea(53));
-console.log(circleArea(0));
-
 //</1>
