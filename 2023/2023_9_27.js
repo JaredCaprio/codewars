0e2ecc0305b60d348a531c560897d6576b3deef1
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-09-27 08:46:14 -0400

    2023-9-27

---
+++ 2023/2023_9_27.js
@@ -0,0 +1,13 @@
+/* <1> https://www.codewars.com/kata/517b25a48557c200b800000c/train/javascript
+Complete the function so that it returns the number of seconds that have elapsed between the start and end times given.
+Tips:
+
+    The start/end times are given as Date (JS/CoffeeScript), DateTime (C#), Time (Nim), datetime(Python) and Time (Ruby) instances.
+    The start time will always be before the end time.
+*/
+
+function elapsedSeconds(startDate, endDate) {
+  return (endDate.getTime() - startDate.getTime()) / 1000;
+}
+
+//</1>
