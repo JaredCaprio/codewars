17639fa1e0fc32988dc05e6a9851e86cf1fe3dcb
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-09-29 10:07:37 -0400

    2023-9-29

---
+++ 2023/2023_9_29.js
@@ -0,0 +1,20 @@
+/* </1> https://www.codewars.com/kata/59dd2c38f703c4ae5e000014/train/javascript
+In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+Good luck!
+
+Please also try Simple remove duplicates
+
+*/
+function solve(s) {
+  let regex = /[0-9]+/g;
+  //use match method on string
+  let matches = s.match(regex);
+
+  //return largest string in array
+  return Math.max(...matches);
+}
+
+//</1>
