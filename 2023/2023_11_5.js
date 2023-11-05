c8fe38213a075da6bf9a2c5b1844c3c9e7e275d6
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-11-05 09:26:13 -0500

    2023_11_5.js

---
+++ 2023/2023_11_5.js
@@ -0,0 +1,23 @@
+/* <1> https://www.codewars.com/kata/592a33e549fe9840a8000ba1/train/javascript
+Is the number even?
+
+If the numbers is even return true. If it's odd, return false.
+
+
+
+
+Oh yeah... the following symbols/commands have been disabled!
+
+    use of %
+    use of .even? in Ruby
+    use of mod in Python
+
+
+*/
+
+var isEven = function (n) {
+  //if n is even return true, otherwise, return false
+  return n & 1 ? false : true;
+};
+
+//</1>
