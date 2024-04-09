21c5a831dfeb01d325c137ee928f5ba372d10166
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2024-04-09 14:00:09 -0400

    2024-4-9

---
+++ 2024/2024_4_9.py
@@ -0,0 +1,19 @@
+"""https://www.codewars.com/kata/551f37452ff852b7bd000139/train/python Binary Addition
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+"""
+
+
+def add_binary(a, b):
+    sum = a + b
+    return format(sum, "b")
+
+
+# test cases
+print(add_binary(23, 12))
