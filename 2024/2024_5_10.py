17c599ac235d2874a274a0af0fae269822208511
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2024-05-10 12:52:33 -0400

    added details

--- 2024/2024_5_10.py
+++ 2024/2024_5_10.py
@@ -1,18 +1,40 @@
-"""  
+"""  https://www.codewars.com/kata/54fe05c4762e2e3047000add OOP: Object Oriented Piracy 
+Ahoy matey!
 
-"""
+You are a leader of a small pirate crew. And you have a plan. With the help of OOP you wish to make a pretty efficient system to identify ships with heavy booty on board!
 
-""" 
-Each time a ship is spotted, a new instance of ship is created
-passing in the draft (how low the ship sits in the water)
-and the crew (number of pirates on board the ship)
+Unfortunately for you, people weigh a lot these days, so how do you know if a ship is full of gold and not people?
 
-a method in the Ship class should be created to determine whether the ship
-is worth looting
-each crew member add 1.5 to the draft value
-so the is_worth_it function subtract the weight added by the crew from the draft
-if the weight of the ship is more than 20 after subtracting the weight of the crew
-then the ship is worth looting
+You begin with writing a generic Ship class / struct:
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+
+Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+
+    draft - an estimate of the ship's weight based on how low it is in the water
+    crew - the count of crew on board
+
+Titanic = Ship(15, 10)
+
+Task
+
+You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+
+Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+
+Add the method
+
+is_worth_it
+
+to decide if the ship is worthy to loot. For example:
+
+Titanic.is_worth_it()
+False
+
+Good luck and may you find GOOOLD!
 
 """
 
